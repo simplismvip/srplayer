3dc548b334eb29a72eb6fefd70e720bedd1577e9
jh <simplismvip@163.com>
2022-07-22 09:17:41 +0800

    update

---
+++ SRPlayer/Classes/Helper/KVOObserver.swift
@@ -0,0 +1,116 @@
+//
+//  RSObserver.swift
+// 
+//  Created by JunMing on 2022/7/18.
+//  Copyright © 2022 JunMing. All rights reserved.
+//
+
+import UIKit
+import ZJMKit
+
+public struct KeyValueOptions: OptionSet {
+    public let rawValue: UInt
+    public init(rawValue: UInt) {
+        self.rawValue = rawValue
+    }
+
+    public static let initial = KeyValueOptions(rawValue: 1 << 0)
+    public static let new = KeyValueOptions(rawValue: 1 << 1)
+    
+    var nsOptions: NSKeyValueObservingOptions {
+        var result: UInt = 0
+        if self.contains(.new) {
+            result |= NSKeyValueObservingOptions.new.rawValue
+        }
+        if self.contains(.initial) {
+            result |= NSKeyValueObservingOptions.initial.rawValue
+        }
+        
+        return NSKeyValueObservingOptions(rawValue: result)
+    }
+}
+
+protocol KVOObservableProtocol {
+    var target: NSObject { get }
+    var keyPath: String { get }
+    var retainTarget: Bool { get }
+    var options: KeyValueOptions { get }
+}
+
+class RSObserver: KVOObserver {
+    typealias Callback = (Any?) -> Void
+    var retainSelf: KVOObserver?
+
+    init(parent: KVOObservableProtocol, callback: @escaping Callback) {
+        super.init(target: parent.target, keyPath: parent.keyPath, retainTarget: parent.retainTarget, options: parent.options.nsOptions, callback: callback)
+        self.retainSelf = self
+    }
+
+    override func deallocObserver() {
+        super.deallocObserver()
+        self.retainSelf = nil
+    }
+
+    deinit {
+        #if TRACE_RESOURCES
+            _ = Resources.decrementTotal()
+        #endif
+    }
+}
+
+final class KVOObservable<Element>: KVOObservableProtocol {
+    typealias Element = Element?
+
+    unowned var target: NSObject
+    var strongTarget: AnyObject?
+
+    var keyPath: String
+    var options: KeyValueOptions
+    var retainTarget: Bool
+
+    init(object: NSObject, keyPath: String, options: KeyValueOptions, retainTarget: Bool) {
+        self.target = object
+        self.keyPath = keyPath
+        self.options = options
+        self.retainTarget = retainTarget
+        if retainTarget {
+            self.strongTarget = object
+        }
+    }
+}
+
+extension NSObject {
+    func observe<Element>(_ type: Element.Type, _ keyPath: String, options: KeyValueOptions = [.new, .initial], retainSelf: Bool = true, callback: @escaping (Any?) -> Void) -> RSObserver {
+        let kvoObservable = KVOObservable<Element>(object: self, keyPath: keyPath, options: options, retainTarget: retainSelf)
+        return RSObserver(parent: kvoObservable, callback: callback)
+    }
+}
+
+@objc class KVOObserver: NSObject {
+    typealias Callback = (Any?) -> Void
+    @objc dynamic var target: NSObject?
+    @objc dynamic var retainedTarget: NSObject?
+    @objc dynamic var keyPath: String = ""
+    @objc dynamic var callback: Callback
+    
+    init(target: NSObject, keyPath: String, retainTarget: Bool, options: NSKeyValueObservingOptions, callback: @escaping Callback ) {
+        self.target = target
+        self.keyPath = keyPath
+        if retainTarget {
+            self.retainedTarget = target
+        }
+        self.callback = callback
+        super.init()
+        self.target?.addObserver(self, forKeyPath: self.keyPath, context: nil)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        self.callback(change?[.newKey])
+    }
+    
+    public func deallocObserver() {
+        self.target?.removeObserver(self, forKeyPath: self.keyPath, context: nil)
+        self.target = nil
+        self.retainedTarget = nil
+    }
+}
